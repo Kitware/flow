8c0b724b4d45957366374fa68da8d8dbdb516129
BetsyMcPhail <betsy.mcphail@kitware.com>
2014-09-22 12:43:46 -0400

    Added interactive heatmap and scatterplot matrix.

---
+++ app/src/plugins/scatterplotmatrix.js
@@ -0,0 +1,28 @@
+(function (tangelo, $, d3) {
+    "use strict";
+
+    if (!($ && $.widget && d3)) {
+        return;
+    }
+
+    tangelo.widget("tangelo.scatterplotmatrix", {
+        options: {
+            data: null,
+            y: null
+        },
+
+        _create: function () {
+            var div = $('<div id="matrix"></div>');
+            this.element.append(div);
+        },
+
+        _update: function () {
+            var matrix = d3.scatterplot.matrix();
+            var data = this.options.data.rows;
+            var y = this.options.y.field;
+            d3.select("#matrix")
+                .datum(data)
+                .call(matrix, y);
+        }
+    });
+}(window.tangelo, window.jQuery, window.d3));
--- app/src/views/VisualizationManagementView.js
+++ app/src/views/VisualizationManagementView.js
@@ -95,6 +95,16 @@
                         dataIsURI: true
                     }
                 ]
+            },
+            {
+                name: "interactiveheatmap",
+                inputs: [{name: "data", type: "table", format: "rows"}]
+            },
+            {
+                name: "scatterplotmatrix",
+                inputs: [{name: "data", type: "table", format: "rows"},
+                         {name: "y", type: "accessor", domain: {input: "data", format: "column.names"}}
+                ]
             }
         ],
 
