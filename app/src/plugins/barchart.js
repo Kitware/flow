03b7d6f4bcbb0ee8fe2ad6d0900b74b7e76ca90a
Jeffrey Baumes <jeff.baumes@kitware.com>
2015-07-09 10:21:50 -0400

    Bar chart

---
+++ app/src/plugins/barchart.js
@@ -0,0 +1,38 @@
+(function ($, tangelo, vg) {
+    "use strict";
+
+    $.fn.barchart = function (spec) {
+        var y = tangelo.accessor(spec.y),
+            x = tangelo.accessor(spec.x),
+            data = spec.data,
+            dt = [],
+            opt = {
+                data: {table: dt},
+                renderer: "svg",
+                el: this[0]
+            },
+            that = this[0];
+
+        data.forEach(function (row) {
+            dt.push({
+                x: x(row),
+                y: _.isNumber(y(row)) ? y(row) : NaN,
+                orig: row
+            });
+        });
+
+        function resize() {
+            vg.parse.spec("barchart.json", function (chart) {
+                console.log($(that).width() + "," + $(that).height());
+                if ($(that).width() > 0 && $(that).height() > 0) {
+                    chart(opt).width($(that).width() - 70).height($(that).height() - 70).update();
+                }
+            });
+        }
+        $(window).resize(resize);
+        resize();
+
+        return that;
+    };
+
+}(window.jQuery, window.tangelo, window.vg));
