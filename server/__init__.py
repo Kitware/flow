d3289cb92abaec14263c3c4fb48f86582b7b2086
Dan LaManna <dan.lamanna@kitware.com>
2016-03-31 15:53:27 -0400

    Support static directory mounting for Flow
    If a directory exists underneath /static then it will be mounted as a
    top level directory under Girder.

--- server/__init__.py
+++ server/__init__.py
@@ -14,3 +14,13 @@ def load(info):
                                                      info['serverRoot'])
 
     info['serverRoot'].api = info['serverRoot'].girder.api
+
+    staticDir = os.path.join(info['pluginRootDir'], 'static')
+    if os.path.isdir(staticDir):
+        for path in os.listdir(staticDir):
+            if os.path.isdir(os.path.join(staticDir, path)):
+                info['config'][str('/' + path)] = {
+                    'tools.staticdir.on': True,
+                    'tools.staticdir.dir': os.path.join(staticDir, path),
+                    'tools.staticdir.index': 'index.html'
+                }
